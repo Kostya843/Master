f962a6eac0595505ab554f9a71388bf80876a0ba
Konstantin Savchenko <savchenkok843@gmail.com>
2025-10-29 22:07:13 +0200

    Revert "добавил if"
    This reverts commit 4d5e7b0ebe6bd842044645fa9a338783ab24c90f.

--- Exercise1/src/Main.java
+++ Exercise1/src/Main.java
@@ -1,8 +1,5 @@
 public static void main(String[] args) {
     System.out.println("args = " + Arrays.toString(args));
     System.out.println("als" + Arrays.toString(args));
-    if(2==2){
-        System.out.println("args = " + Arrays.toString(args));
-    }
 
 }
