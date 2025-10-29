4d5e7b0ebe6bd842044645fa9a338783ab24c90f
Konstantin Savchenko <savchenkok843@gmail.com>
2025-10-29 22:00:55 +0200

    добавил if

--- Exercise1/src/Main.java
+++ Exercise1/src/Main.java
@@ -1,5 +1,8 @@
 public static void main(String[] args) {
     System.out.println("args = " + Arrays.toString(args));
     System.out.println("als" + Arrays.toString(args));
+    if(2==2){
+        System.out.println("args = " + Arrays.toString(args));
+    }
 
 }
