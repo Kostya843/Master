46d4c3b8bf4e1b4682ddac976f02eb4f02d2a944
Konstantin Savchenko <savchenkok843@gmail.com>
2025-10-29 22:08:55 +0200

    fdasfddw

--- Exercise1/src/Main.java
+++ Exercise1/src/Main.java
@@ -1,8 +1,11 @@
 public static void main(String[] args) {
     System.out.println("args = " + Arrays.toString(args));
+<<<<<<< HEAD
     System.out.println("als" + Arrays.toString(args));
     if(2==2){
         System.out.println("args = " + Arrays.toString(args));
     }
 
+=======
+>>>>>>> parent of e354111 (добавил текст)
 }
